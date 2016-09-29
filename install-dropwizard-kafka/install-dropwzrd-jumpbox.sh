97073af6f387a7221f4d9bb6ab95638110401839
srilatha.pasupula <srilatha.pasupula@gmail.com>
2016-09-29 15:58:34 +0530

    install-dropwizard-jumpbox

---
+++ install-dropwizard-kafka/install-dropwzrd-jumpbox.sh
@@ -0,0 +1,19 @@
+
+
+# Install Oracle Java 
+ log "Installing Java" 
+ 
+ 
+ # redhat java install 
+ cd /tmp 
+ wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm" 
+ wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-linux-x64.rpm" 
+ 
+ 
+ #Install packages 
+ rpm -Uvh jdk-8u102-linux-x64.rpm 
+ rpm -Uvh jre-8u102-linux-x64.rpm 
+ 
+ 
+cd /tmp
+ $java -jar target/dropwizard-kafka-http-0.0.1-SNAPSHOT.jar server kafka-http.yml
