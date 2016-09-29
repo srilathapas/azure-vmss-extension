17303a742975e00595edbb52f7f141e2f84e2bfd
srilatha.pasupula <srilatha.pasupula@gmail.com>
2016-09-29 16:18:05 +0530

    removed spaces

--- install-dropwizard-kafka/install-dropwzrd-jumpbox.sh
+++ install-dropwizard-kafka/install-dropwzrd-jumpbox.sh
@@ -1,19 +1,10 @@
-
-
-# Install Oracle Java 
- log "Installing Java" 
- 
- 
  # redhat java install 
  cd /tmp 
  wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm" 
  wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-linux-x64.rpm" 
- 
- 
  #Install packages 
  rpm -Uvh jdk-8u102-linux-x64.rpm 
  rpm -Uvh jre-8u102-linux-x64.rpm 
- 
  cd /tmp 
  git clone https://github.com/srilathapas/azure-vmss-extension/tree/master/install-dropwizard-kafka
 cd /tmp/install-dropwizard-kafka
