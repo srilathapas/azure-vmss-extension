aecd4f7dd92bc79d3d18e859344f925587fbd7a7
srilatha.pasupula <srilatha.pasupula@gmail.com>
2016-09-29 16:27:22 +0530

    s

--- install-dropwizard-kafka/install-dropwzrd-jumpbox.sh
+++ install-dropwizard-kafka/install-dropwzrd-jumpbox.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
  # redhat java install 
  cd /tmp 
  wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm" 
