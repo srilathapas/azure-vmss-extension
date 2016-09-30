a4c661b97a684223031d0b2fdea5b5a3c826ae8a
srilatha.pasupula <srilatha.pasupula@gmail.com>
2016-09-30 10:19:53 +0530

    s

--- install-dropwizard-kafka/install-dropwzrd-jumpbox.sh
+++ install-dropwizard-kafka/install-dropwzrd-jumpbox.sh
@@ -7,9 +7,9 @@ wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2F
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-linux-x64.rpm" 
 
 #Install packages 
-rpm -Uvh jdk-8u102-linux-x64.rpm 
-rpm -Uvh jre-8u102-linux-x64.rpm 
+rpm -ivh jdk-8u102-linux-x64.rpm 
+rpm -ivh jre-8u102-linux-x64.rpm 
 
 wget "https://github.com/srilathapas/azure-vmss-extension/tree/master/install-dropwizard-kafka/kafka-http.yml"
 cd /tmp
-$Java -jar target/dropwizard-kafka-http-0.0.1-SNAPSHOT.jar server kafka-http.yml
+java -jar target/dropwizard-kafka-http-0.0.1-SNAPSHOT.jar server kafka-http.yml
